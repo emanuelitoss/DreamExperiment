fab94144c83891d66fdae75561f210f8b0457c03
Emanuele <rosi.1812180@studenti.uniroma1.it>
2022-04-15 19:10:17 +0200

    Files renamed

--- dream_simulation.cc
+++ dream_simulation.cc
@@ -27,8 +27,8 @@
 /// \file exampleB1.cc
 /// \brief Main program of the B1 example
 
-#include "B1DetectorConstruction.hh"
-#include "B1ActionInitialization.hh"
+#include "DetectorConstruction.hh"
+#include "ActionInitialization.hh"
 
 #include "G4RunManagerFactory.hh"
 
