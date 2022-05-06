5489593dd7769508986d153e9c7cbf806acdf673
Emanuele <rosi.1812180@studenti.uniroma1.it>
2022-05-06 19:54:43 +0200

    Spherical world & envel.

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -40,6 +40,7 @@
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
 
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 DetectorConstruction::DetectorConstruction()
@@ -58,26 +59,32 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 {  
   // Get nist material manager
   G4NistManager* nist = G4NistManager::Instance();
+
+  //
+  // Dimensions BGO and plastic scintillators
+  //
+  G4double shape_bgoXZ = 2.2*cm, shape_bgoY = 18*cm;
+  G4double shape_plasticX = 5*cm, shape_plasticY = 1.5*cm, shape_plasticZ = 10*cm;
+  G4double distance_scintillators = 0.5*cm;
+  G4double distance_BGOscintillators = 10*cm;
+  G4double H = 2 * shape_plasticZ + distance_BGOscintillators + distance_scintillators;
+
+  G4double radius = (4 * H*H + shape_bgoY*shape_bgoY) / (8*H);
+  G4double radius_sphere = radius * 1.2;
   
   // Envelope parameters
-  //
-  G4double env_sizeXZ = 25*cm, env_sizeY = 65*cm;
   G4Material* env_mat = nist->FindOrBuildMaterial("G4_AIR");
    
   // Option to switch on/off checking of volumes overlaps
-  //
   G4bool checkOverlaps = true;
 
   //     
   // World
   //
-  G4double world_sizeXZ = 1.2*env_sizeXZ;
-  G4double world_sizeY  = 1.2*env_sizeY;
   G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
   
-  G4Box* solidWorld =    
-    new G4Box("World",                       //its name
-       0.5*world_sizeXZ, 0.5*world_sizeY, 0.5*world_sizeXZ);     //its size
+  G4Sphere* solidWorld = 
+    new G4Sphere("World", 0, radius_sphere, 0.*deg, 360.*deg, 0.*deg, 180.*deg);
       
   G4LogicalVolume* logicWorld =                         
     new G4LogicalVolume(solidWorld,          //its solid
@@ -97,9 +104,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   //     
   // Envelope
   //  
-  G4Box* solidEnv =    
-    new G4Box("Envelope",                    //its name
-        0.5*env_sizeXZ, 0.5*env_sizeY, 0.5*env_sizeXZ); //its size
+  G4Sphere* solidEnv = new G4Sphere("World", 0, radius_sphere, 0.*deg, 360.*deg, 0.*deg, 180.*deg);
       
   G4LogicalVolume* logicEnv =                         
     new G4LogicalVolume(solidEnv,            //its solid
@@ -114,80 +119,18 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
-  /*
-  //     
-  // Shape 1
-  //  
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
-        
-  // Conical section shape       
-  G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
-  G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
-  G4double shape1_hz = 3.*cm;
-  G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
-  G4Cons* solidShape1 =    
-    new G4Cons("Shape1", 
-    shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
-    shape1_phimin, shape1_phimax);
-                      
-  G4LogicalVolume* logicShape1 =                         
-    new G4LogicalVolume(solidShape1,         //its solid
-                        shape1_mat,          //its material
-                        "Shape1");           //its name
-               
-  new G4PVPlacement(0,                       //no rotation
-                    pos1,                    //at position
-                    logicShape1,             //its logical volume
-                    "Shape1",                //its name
-                    logicEnv,                //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
 
-  //     
-  // Shape 2
-  //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1*cm, 7*cm);
-
-  // Trapezoid shape       
-  G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
-  G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
-  G4double shape2_dz  = 6*cm;      
-  G4Trd* solidShape2 =    
-    new G4Trd("Shape2",                      //its name
-              0.5*shape2_dxa, 0.5*shape2_dxb, 
-              0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz); //its size
-                
-  G4LogicalVolume* logicShape2 =                         
-    new G4LogicalVolume(solidShape2,         //its solid
-                        shape2_mat,          //its material
-                        "Shape2");           //its name
-               
-  new G4PVPlacement(0,                       //no rotation
-                    pos2,                    //at position
-                    logicShape2,             //its logical volume
-                    "Shape2",                //its name
-                    logicEnv,                //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
-                
-  // Set Shape2 as scoring volume
-  //
-  fScoringVolume = logicShape2;
-  */
 
   //     
   // BGO Crystal bgo_material
   //  
+  G4double pos_BGO = radius - 2 * shape_plasticZ - distance_BGOscintillators - distance_scintillators - shape_bgoXZ / 2;
   G4Material* bgo_material = nist->FindOrBuildMaterial("G4_BGO");
-  G4ThreeVector bgo_position = G4ThreeVector(0, 0, 0);
+  G4ThreeVector bgo_position = G4ThreeVector(0, 0, pos_BGO);
+  
         
-  // BGO shape      
-  G4double shape_bgoYZ =  2.2*cm, shape_bgoX = 18*cm;
-  G4Box* BGOShape = new G4Box("BGO Box", 0.5*shape_bgoX, 0.5*shape_bgoYZ, 0.5*shape_bgoYZ);
+  // BGO shape
+  G4Box* BGOShape = new G4Box("BGO Box", 0.5*shape_bgoXZ, 0.5*shape_bgoY, 0.5*shape_bgoXZ);
                       
   G4LogicalVolume* BGO_LogicalVolume =                         
     new G4LogicalVolume(BGOShape,            //its solid
@@ -205,19 +148,16 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   //     
   // Plastic scintillator
   //  
+  G4double pos_scint1 = radius - distance_scintillators - 1.5 * shape_plasticZ;
+  G4double pos_scint2 = radius - shape_plasticZ / 2;
   G4Material* plastic_material = nist->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
         
-  // Plastic scintillators shape      
-  G4double shape_plasticX = 1.5*cm, shape_plasticY = 10*cm, shape_plasticZ = 5*cm;
+  // Plastic scintillators shape
   G4Box* PlasticShape = new G4Box("BGO Box", 0.5*shape_plasticX, 0.5*shape_plasticY, 0.5*shape_plasticZ);
 
-  // Distance between center of BGO and the nearest side of the first plastic scintillator
-  G4double fixed_distance = 10*cm;
-  G4double distance_between_plastics = 0.3*cm;
-
   // Plastic scintillators positions
-  G4ThreeVector trigger_1 = G4ThreeVector(0, -fixed_distance-0.5*shape_plasticY+0.5*distance_between_plastics, 0);
-  G4ThreeVector trigger_2 = G4ThreeVector(0, -fixed_distance-1.5*shape_plasticY-0.5*distance_between_plastics, 0);
+  G4ThreeVector trigger_1 = G4ThreeVector(0, 0, pos_scint1);
+  G4ThreeVector trigger_2 = G4ThreeVector(0, 0, pos_scint2);
 
   G4LogicalVolume* Plastic_LogicalVolume =                         
     new G4LogicalVolume(PlasticShape,             //its solid
