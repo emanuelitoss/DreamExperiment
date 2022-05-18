fdfca791d773fb6728689286668b24e5286c2a9a
Emanuele <rosi.1812180@studenti.uniroma1.it>
2022-05-18 10:58:18 +0200

    Taking into account Energy deposits

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -30,7 +30,7 @@
 #include "../include/RunAction.hh"
 #include "../include/PrimaryGeneratorAction.hh"
 #include "../include/DetectorConstruction.hh"
-// #include "Run.hh"
+#include "../include/RunData.hh"
 
 #include "G4RunManager.hh"
 #include "G4Run.hh"
@@ -39,41 +39,87 @@
 #include "G4LogicalVolume.hh"
 #include "G4UnitsTable.hh"
 #include "G4SystemOfUnits.hh"
+#include "g4root.hh"
+
 
 RunAction::RunAction()
 : G4UserRunAction(),
   fEdep(0.),
   fEdep2(0.)
-{ 
+{
   // add new units for dose
-  // 
   const G4double milligray = 1.e-3*gray;
   const G4double microgray = 1.e-6*gray;
-  const G4double nanogray  = 1.e-9*gray;  
+  const G4double nanogray  = 1.e-9*gray;
   const G4double picogray  = 1.e-12*gray;
-   
+
   new G4UnitDefinition("milligray", "milliGy" , "Dose", milligray);
   new G4UnitDefinition("microgray", "microGy" , "Dose", microgray);
   new G4UnitDefinition("nanogray" , "nanoGy"  , "Dose", nanogray);
-  new G4UnitDefinition("picogray" , "picoGy"  , "Dose", picogray); 
+  new G4UnitDefinition("picogray" , "picoGy"  , "Dose", picogray);
+
+  // set printing event number per each event
+  // G4RunManager::GetRunManager()->SetPrintProgress(1);
+
+  // Create analysis manager
+  // The choice of analysis technology is done via selectin of a namespace
+  // in Analysis.hh
+  auto analysisManager = G4AnalysisManager::Instance();
+  G4cout << "Using " << analysisManager->GetType() << G4endl;
+
+  // Create directories
+  //analysisManager->SetHistoDirectoryName("histograms");
+  //analysisManager->SetNtupleDirectoryName("ntuple");
+  analysisManager->SetVerboseLevel(1);
+  analysisManager->SetNtupleMerging(true);
+  // Note: merging ntuples is available only with Root output
+
+  // Creating histograms
+  analysisManager->CreateH1("EnergyBGO","Edep in BGO crystal", 100, 0., 10*GeV);
+  analysisManager->CreateH1("EnergyTRG1","Edep in first plastic scintillator", 100, 0., 10*GeV);
+  analysisManager->CreateH1("EnergyTRG2","Edep in second plastic scintillator", 100, 0., 10*GeV);
+
+  // Creating ntuple
+  analysisManager->CreateNtuple("dreamData", "EnengyDeposit");
+  analysisManager->CreateNtupleDColumn("EnergyInBGO");
+  analysisManager->CreateNtupleDColumn("EnergyInScintillator1");
+  analysisManager->CreateNtupleDColumn("EnergyInScintillator2");
+  analysisManager->FinishNtuple();
 
   // Register accumulable to the accumulable manager
   G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
   accumulableManager->RegisterAccumulable(fEdep);
   accumulableManager->RegisterAccumulable(fEdep2); 
+  
 }
 
-RunAction::~RunAction(){}
+RunAction::~RunAction()
+{
+  delete G4AnalysisManager::Instance();  
+}
 
-void RunAction::BeginOfRunAction(const G4Run*)
-{ 
+G4Run* RunAction::GenerateRun()
+{
+  return (new RunData);
+}
+
+void RunAction::BeginOfRunAction(const G4Run* run)
+{
+  G4cout << "### Run " << run->GetRunID() << " start." << G4endl;
+  
   // inform the runManager to save random number seed
   G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+
+  // Get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Open an output file
+  G4String fileName = "../analysisDreamSimulation/data_dreamExperiment.root";
+  analysisManager->OpenFile(fileName);
 
   // reset accumulables to their initial values
   G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
   accumulableManager->Reset();
-
 }
 
 void RunAction::EndOfRunAction(const G4Run* run){
@@ -86,7 +132,6 @@ void RunAction::EndOfRunAction(const G4Run* run){
   accumulableManager->Merge();
 
   // Compute dose = total energy deposit in a run and its variance
-  //
   G4double edep  = fEdep.GetValue();
   G4double edep2 = fEdep2.GetValue();
   
@@ -95,7 +140,7 @@ void RunAction::EndOfRunAction(const G4Run* run){
 
   const DetectorConstruction* detectorConstruction
    = static_cast<const DetectorConstruction*>
-     (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+    (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
   G4double mass = detectorConstruction->GetScoringVolume()->GetMass();
   G4double dose = edep/mass;
   G4double rmsDose = rms/mass;
@@ -104,8 +149,8 @@ void RunAction::EndOfRunAction(const G4Run* run){
   //  note: There is no primary generator action object for "master"
   //        run manager for multi-threaded mode.
   const PrimaryGeneratorAction* generatorAction
-   = static_cast<const PrimaryGeneratorAction*>
-     (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+    = static_cast<const PrimaryGeneratorAction*>
+    (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
   G4String runCondition;
   if (generatorAction)
   {
@@ -139,6 +184,39 @@ void RunAction::EndOfRunAction(const G4Run* run){
      << "------------------------------------------------------------"
      << G4endl
      << G4endl;
+
+  // print histogram statistics
+  auto analysisManager = G4AnalysisManager::Instance();
+  if ( analysisManager->GetH1(1) ) {
+    G4cout << G4endl << " ----> print histograms statistic ";
+    if(isMaster) {
+      G4cout << "for the entire run " << G4endl << G4endl;
+    }
+    else {
+      G4cout << "for the local thread " << G4endl << G4endl;
+    }
+  
+    G4cout << " EnergyBGO : mean = "
+      << G4BestUnit(analysisManager->GetH1(0)->mean(), "Energy")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(0)->rms(),  "Energy") << G4endl;
+  
+    G4cout << " EnergyScintillator1 : mean = "
+      << G4BestUnit(analysisManager->GetH1(1)->mean(), "Energy")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(1)->rms(),  "Energy") << G4endl;
+
+    G4cout << " EnergyScintillator2 : mean = "
+      << G4BestUnit(analysisManager->GetH1(2)->mean(), "Energy")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(2)->rms(),  "Energy") << G4endl;
+
+  }
+
+  // save histograms & ntuple
+  analysisManager->Write();
+  analysisManager->CloseFile();
+
 }
 
 void RunAction::AddEdep(G4double edep){
