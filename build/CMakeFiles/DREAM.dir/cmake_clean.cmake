02c2d8283ef3cca0c3d7efa3ca9046ee2cecc436
Emanuele <91272890+emanuelitoss@users.noreply.github.com>
2022-05-26 12:05:29 +0200

    Delete build directory

--- build/CMakeFiles/DREAM.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DREAM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DREAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
